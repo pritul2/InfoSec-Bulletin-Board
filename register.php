b24256688aee3042fcbc68153c2b583aa6698aaa
Pritul Dave <pritul.dave@gmail.com>
2023-04-22 18:06:09 -0500

    Update register.php

--- register.php
+++ register.php
@@ -5,48 +5,44 @@
 <title>hackme</title>
 <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
 <?php
-include ('connect.php');
-include ('header.php');
-connect();
+	include('connect.php');
+	include('header.php');
+	connect();
 ?>
 <div class="post">
 	<div class="post-bgtop">
 		<div class="post-bgbtm">
         <h2 class = "title">hackme Registration</h2>
         <?php
-//if the registration form is submitted
-if (isset($_POST['submit']))
-{
-
-    $_POST['uname'] = trim($_POST['uname']);
-    if (!$_POST['uname'] | !$_POST['password'] | !$_POST['fname'] | !$_POST['lname'])
-    {
-        die('<p>You did not fill in a required field.
+		//if the registration form is submitted 
+		if (isset($_POST['submit'])) {
+			
+			$_POST['uname'] = trim($_POST['uname']);
+			if(!$_POST['uname'] | !$_POST['password'] |
+				!$_POST['fname'] | !$_POST['lname']) {
+ 				die('<p>You did not fill in a required field.
 				Please go back and try again!</p>');
-    }
-
-    $passwordHash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-    $check = mysql_query("SELECT * FROM users WHERE username = '" . $_POST['uname'] . "'") or die(mysql_error());
-
-    //Gives error if user already exist
-    $check2 = mysql_num_rows($check);
-    if ($check2 != 0)
-    {
-        die('<p>Sorry, user name already exists.</p>');
-    }
-    else
-    {
-        mysql_query("INSERT INTO users (username, pass, fname, lname) VALUES ('" . $_POST['uname'] . "', '" . $passwordHash . "', '" . $_POST['fname'] . "', '" . $_POST['lname'] . "');") or die(mysql_error());
-
-        echo "<h3> Registration Successful!</h3> <p>Welcome " . $_POST['fname'] . "! Please log in...</p>";
-    }
-?>    
+ 			}
+		
+			$passwordHash = hash('sha256', $_POST['password']);
+			
+			$check = mysql_query("SELECT * FROM users WHERE username = '".$_POST['uname']."'")or die(mysql_error());
+ 
+ 		//Gives error if user already exist
+ 		$check2 = mysql_num_rows($check);
+		if ($check2 != 0) {
+			die('<p>Sorry, user name already exists.</p>');
+		}
+		else
+		{
+			mysql_query("INSERT INTO users (username, pass, fname, lname) VALUES ('".$_POST['uname']."', '". $passwordHash ."', '". $_POST['fname']."', '". $_POST['lname'] ."');")or die(mysql_error());
+			
+			echo "<h3> Registration Successful!</h3> <p>Welcome ". $_POST['fname'] ."! Please log in...</p>";
+		} 
+        ?>    
         <?php
-}
-else
-{
-?>
+		}else{
+        ?>
         	<form  method="post" action="register.php">
             <table>
                 <tr>
@@ -72,13 +68,13 @@ else
             </table>
             </form>
         <?php
-}
-?>
+		}
+		?>
         </div>
     </div>
 </div>
 <?php
-include ('footer.php');
+	include('footer.php');
 ?>
 </body>
 </html>
