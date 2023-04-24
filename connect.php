23a5d49aea2d550d727b549676f2efe29a0a62c0
Pritul Dave <pritul.dave@gmail.com>
2023-04-24 11:20:27 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 	function connect()
 	{
 		// Connects to the Database 
-		mysql_connect("127.0.0.1", "cs6324spring22", "3Q9Y5Asg8as8p5m4", false, 3306) or die(mysql_error());
-		mysql_select_db("cs6324spring22") or die(mysql_error());
+		mysql_connect("127.0.0.1", "cs6324spring23", "8mwni8URcsr08bCxx", false, 65536) or die(mysql_error());
+		mysql_select_db("cs6324spring23") or die(mysql_error());
 	}
 ?>
